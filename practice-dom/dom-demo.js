b6a1d44557ca74555f7c8cbab26ae6748afc43d8
G184372021 <218437@st.takushoku-u.ac.jp>
2022-05-19 12:27:58 +0900

    a

--- practice-dom/dom-demo.js
+++ practice-dom/dom-demo.js
@@ -9,5 +9,9 @@ ul.insertAdjacentElement('beforeend', li);
 li = document.createElement('li');
 li.textContent = 'ヨーロッパ大陸';
 ul.insertAdjacentElement('beforeend', li);
-
+let h2s = document.querySelectorAll('h2');
+for (let h2 of h2s) {
+    h1.style.backgroundColor = 'palegreen';
+    h1.style.fontSize = '20pt'
+}
 
